5640986fcbe151c92b8c7f803cd2d9cfaa18d972
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-22 23:35:12 +0500

    Задача**

---
+++ Задача_степень/Program.cs
@@ -0,0 +1,12 @@
+﻿double a;
+double Stepen;
+Console.WriteLine("Введите число  ");
+a = Convert.ToDouble(Console.ReadLine());
+if(a==0)
+{
+    Console.WriteLine("Нельзя делить на ноль");
+}
+Stepen = 1/a; //double pow = Math.Pow(a,-1); 
+Console.WriteLine(Stepen);
+
+          
